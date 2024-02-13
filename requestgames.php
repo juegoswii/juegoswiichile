eaee51283ddede23818aa4e163f7dd0123539c75
juegoswii <156217656+juegoswii@users.noreply.github.com>
2024-02-13 00:55:06 -0300

    Add files via upload

---
+++ requestgames.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $nombrejuego = $_POST["nombrejuego"];
+    $opciones = $_POST["opciones"];
+    $email = $_POST["email"];
+
+    $destinatario = "juegoswiichile@chilehero.cl";
+    
+    $asunto = "Pedido de $nombre";
+
+    $cuerpo = "Artista: $artista\n";
+    $cuerpo .= "Cancion:$cancion\n";
+    $cuerpo .= "Consola: $opciones\n";
+    $cuerpo .= "Correo: $email\n";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    $alerta = "Gracias por pedir tu juego";
+    
+    mail($destinatario, $asunto, $cuerpo, $headers);
+
+    echo "<script type='text/javascript'>alert('$alerta');</script>";
+    exit();
+}
+?>
